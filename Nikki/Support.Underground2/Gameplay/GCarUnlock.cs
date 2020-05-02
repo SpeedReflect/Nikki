5a2515869c776cc84022bf2d73f1894b87e0bc28
MaxHwoy <max.hwoy@gmail.com>
2020-05-01 19:26:08 -0700

    Added GCarUnlock and GShowcase classes to Underground2 support.

--- Nikki/Support.Underground2/Gameplay/GCarUnlock.cs
+++ Nikki/Support.Underground2/Gameplay/GCarUnlock.cs
@@ -5,16 +5,15 @@ using Nikki.Utils;
 using Nikki.Reflection.Abstract;
 using Nikki.Reflection.Exception;
 using Nikki.Reflection.Attributes;
-using CoreExtensions.IO;
 
 
 
 namespace Nikki.Support.Underground2.Gameplay
 {
 	/// <summary>
-	/// <see cref="GCareerBrand"/> is a collection of settings related to sponsor brands.
+	/// <see cref="GCarUnlock"/> is a collection of settings related to car unlock requirements.
 	/// </summary>
-	public class GCareerBrand : ACollectable
+	public class GCarUnlock : ACollectable
 	{
 		#region Fields
 
@@ -52,9 +51,7 @@ namespace Nikki.Support.Underground2.Gameplay
 					throw new ArgumentNullException("This value cannot be left left empty.");
 				if (value.Contains(" "))
 					throw new Exception("CollectionName cannot contain whitespace.");
-				if (value.Length > 0x1F)
-					throw new ArgumentLengthException("Length of the value should not exceed 31 character.");
-				if (this.Database.GCareerBrands.FindCollection(value) != null)
+				if (this.Database.GCarUnlocks.FindCollection(value) != null)
 					throw new CollectionExistenceException();
 				this._collection_name = value;
 			}
@@ -71,26 +68,34 @@ namespace Nikki.Support.Underground2.Gameplay
 		public uint VltKey => this._collection_name.VltHash();
 
 		/// <summary>
-		/// In-Game name of this <see cref="GCareerBrand"/>.
+		/// One of the two required events completed to unlock the car.
 		/// </summary>
 		[AccessModifiable()]
-		public string IngameBrandName { get; set; } = String.Empty;
+		[StaticModifiable()]
+		public string ReqEventCompleted1 { get; set; }
+
+		/// <summary>
+		/// One of the two required events completed to unlock the car.
+		/// </summary>
+		[AccessModifiable()]
+		[StaticModifiable()]
+		public string ReqEventCompleted2 { get; set; }
 
 		#endregion
 
 		#region Main
 
 		/// <summary>
-		/// Initializes new instance of <see cref="GCareerBrand"/>.
+		/// Initializes new instance of <see cref="GCarUnlock"/>.
 		/// </summary>
-		public GCareerBrand() { }
+		public GCarUnlock() { }
 
 		/// <summary>
-		/// Initializes new instance of <see cref="GCareerBrand"/>.
+		/// Initializes new instance of <see cref="GCarUnlock"/>.
 		/// </summary>
 		/// <param name="CName">CollectionName of the new instance.</param>
 		/// <param name="db"><see cref="Database.Underground2"/> to which this instance belongs to.</param>
-		public GCareerBrand(string CName, Database.Underground2 db)
+		public GCarUnlock(string CName, Database.Underground2 db)
 		{
 			this.Database = db;
 			this.CollectionName = CName;
@@ -98,11 +103,11 @@ namespace Nikki.Support.Underground2.Gameplay
 		}
 
 		/// <summary>
-		/// Initializes new instance of <see cref="GCareerBrand"/>.
+		/// Initializes new instance of <see cref="GCarUnlock"/>.
 		/// </summary>
 		/// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
 		/// <param name="db"><see cref="Database.Underground2"/> to which this instance belongs to.</param>
-		public GCareerBrand(BinaryReader br, Database.Underground2 db)
+		public unsafe GCarUnlock(BinaryReader br, Database.Underground2 db)
 		{
 			this.Database = db;
 			this.Disassemble(br);
@@ -111,39 +116,32 @@ namespace Nikki.Support.Underground2.Gameplay
 		/// <summary>
 		/// Destroys current instance.
 		/// </summary>
-		~GCareerBrand() { }
+		~GCarUnlock() { }
 
 		#endregion
 
 		#region Methods
 
 		/// <summary>
-		/// Assembles <see cref="GCareerBrand"/> into a byte array.
+		/// Assembles <see cref="GCarUnlock"/> into a byte array.
 		/// </summary>
-		/// <param name="bw"><see cref="BinaryWriter"/> to write <see cref="GCareerBrand"/> with.</param>
-		/// <param name="strw"><see cref="BinaryWriter"/> to write strings with.</param>
-		public void Assemble(BinaryWriter bw, BinaryWriter strw)
+		/// <param name="bw"><see cref="BinaryWriter"/> to write <see cref="GCarUnlock"/> with.</param>
+		public void Assemble(BinaryWriter bw)
 		{
-			strw.WriteNullTermUTF8(this._collection_name);
-
-			bw.WriteNullTermUTF8(this._collection_name, 0x20);
-			bw.WriteNullTermUTF8(this.IngameBrandName, 0x20);
 			bw.Write(this.BinKey);
+			bw.Write(this.ReqEventCompleted1.BinHash());
+			bw.Write(this.ReqEventCompleted2.BinHash());
 		}
 
 		/// <summary>
-		/// Disassembles array into <see cref="GCareerStage"/> properties.
+		/// Disassembles array into <see cref="GCarUnlock"/> properties.
 		/// </summary>
-		/// <param name="br"><see cref="BinaryReader"/> to read <see cref="GCareerBrand"/> with.</param>
+		/// <param name="br"><see cref="BinaryReader"/> to read <see cref="GCarUnlock"/> with.</param>
 		public void Disassemble(BinaryReader br)
 		{
-			// CollectionName
-			this._collection_name = br.ReadNullTermUTF8(0x20);
-
-			// Ingame Brand Name
-			this.IngameBrandName = br.ReadNullTermUTF8(0x20);
-
-			br.BaseStream.Position += 4;
+			this._collection_name = br.ReadUInt32().BinString(eLookupReturn.EMPTY);
+			this.ReqEventCompleted1 = br.ReadUInt32().BinString(eLookupReturn.EMPTY);
+			this.ReqEventCompleted2 = br.ReadUInt32().BinString(eLookupReturn.EMPTY);
 		}
 
 		/// <summary>
@@ -153,16 +151,17 @@ namespace Nikki.Support.Underground2.Gameplay
 		/// <returns>Memory casted copy of the object.</returns>
 		public override ACollectable MemoryCast(string CName)
 		{
-			var result = new GCareerBrand(CName, this.Database)
+			var result = new GCarUnlock(CName, this.Database)
 			{
-				IngameBrandName = this.IngameBrandName
+				ReqEventCompleted1 = this.ReqEventCompleted1,
+				ReqEventCompleted2 = this.ReqEventCompleted2
 			};
 
 			return result;
 		}
 
 		/// <summary>
-		/// Returns CollectionName, BinKey and GameSTR of this <see cref="GCareerBrand"/> 
+		/// Returns CollectionName, BinKey and GameSTR of this <see cref="BankTrigger"/> 
 		/// as a string value.
 		/// </summary>
 		/// <returns>String value.</returns>
---
+++ Nikki/Support.Underground2/Gameplay/GShowcase.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.IO;
+using Nikki.Core;
+using Nikki.Utils;
+using Nikki.Reflection.Enum;
+using Nikki.Reflection.Abstract;
+using Nikki.Reflection.Exception;
+using Nikki.Reflection.Attributes;
+using CoreExtensions.IO;
+
+
+
+namespace Nikki.Support.Underground2.Gameplay
+{
+	/// <summary>
+	/// <see cref="GShowcase"/> is a collection of settings related to showcase events.
+	/// </summary>
+	public class GShowcase : ACollectable
+	{
+		#region Fields
+
+		private string _collection_name;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Game to which the class belongs to.
+		/// </summary>
+		public override GameINT GameINT => GameINT.Underground2;
+
+		/// <summary>
+		/// Game string to which the class belongs to.
+		/// </summary>
+		public override string GameSTR => GameINT.Underground2.ToString();
+
+		/// <summary>
+		/// Database to which the class belongs to.
+		/// </summary>
+		public Database.Underground2 Database { get; set; }
+
+		/// <summary>
+		/// Collection name of the variable.
+		/// </summary>
+		[AccessModifiable()]
+		public override string CollectionName
+		{
+			get => this._collection_name;
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+					throw new ArgumentNullException("This value cannot be left left empty.");
+				if (value.Contains(" "))
+					throw new Exception("CollectionName cannot contain whitespace.");
+				if (value.Length > 0x1F)
+					throw new ArgumentLengthException(0x1F);
+				if (this.Database.GShowcases.FindCollection(value) != null)
+					throw new CollectionExistenceException();
+				this._collection_name = value;
+			}
+		}
+
+		/// <summary>
+		/// Binary memory hash of the collection name.
+		/// </summary>
+		public uint BinKey => this._collection_name.BinHash();
+
+		/// <summary>
+		/// Vault memory hash of the collection name.
+		/// </summary>
+		public uint VltKey => this._collection_name.VltHash();
+
+		/// <summary>
+		/// Description string for the showcase message.
+		/// </summary>
+		[AccessModifiable()]
+		public string DescStringLabel { get; set; } = String.Empty;
+
+		/// <summary>
+		/// Destination trigger of the showcase.
+		/// </summary>
+		[AccessModifiable()]
+		public string DestinationPoint { get; set; } = String.Empty;
+
+		/// <summary>
+		/// Descriptive attribute of the showcase.
+		/// </summary>
+		[AccessModifiable()]
+		public string DescAttrib { get; set; } = String.Empty;
+
+		/// <summary>
+		/// Method of taking photo.
+		/// </summary>
+		[AccessModifiable()]
+		[StaticModifiable()]
+		public eTakePhotoMethod TakePhotoMethod { get; set; }
+
+		/// <summary>
+		/// Stage to which this <see cref="GShowcase"/> belongs to.
+		/// </summary>
+		[AccessModifiable()]
+		[StaticModifiable()]
+		public byte BelongsToStage { get; set; }
+
+		/// <summary>
+		/// Cash value player gets from completing the showcase.
+		/// </summary>
+		[AccessModifiable()]
+		[StaticModifiable()]
+		public short CashValue { get; set; }
+
+		/// <summary>
+		/// Required visual rating of a car to unlock this <see cref="GShowcase"/>.
+		/// </summary>
+		[AccessModifiable()]
+		[StaticModifiable()]
+		public float RequiredVisualRating { get; set; }
+
+		/// <summary>
+		/// Unknown value at offset 0x34.
+		/// </summary>
+		[AccessModifiable()]
+		public byte Unknown0x34 { get; set; }
+
+		/// <summary>
+		/// Unknown value at offset 0x35.
+		/// </summary>
+		[AccessModifiable()]
+		public byte Unknown0x35 { get; set; }
+
+		#endregion
+
+		#region Main
+
+		/// <summary>
+		/// Initializes new instance of <see cref="GShowcase"/>.
+		/// </summary>
+		public GShowcase() { }
+
+		/// <summary>
+		/// Initializes new instance of <see cref="GShowcase"/>.
+		/// </summary>
+		/// <param name="CName">CollectionName of the new instance.</param>
+		/// <param name="db"><see cref="Database.Underground2"/> to which this instance belongs to.</param>
+		public GShowcase(string CName, Database.Underground2 db)
+		{
+			this.Database = db;
+			this.CollectionName = CName;
+			CName.BinHash();
+		}
+
+		/// <summary>
+		/// Initializes new instance of <see cref="GShowcase"/>.
+		/// </summary>
+		/// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
+		/// <param name="db"><see cref="Database.Underground2"/> to which this instance belongs to.</param>
+		public unsafe GShowcase(BinaryReader br, Database.Underground2 db)
+		{
+			this.Database = db;
+			this.Disassemble(br);
+		}
+
+		/// <summary>
+		/// Destroys current instance.
+		/// </summary>
+		~GShowcase() { }
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Assembles <see cref="GShowcase"/> into a byte array.
+		/// </summary>
+		/// <param name="bw"><see cref="BinaryWriter"/> to write <see cref="GShowcase"/> with.</param>
+		/// <param name="strw"><see cref="BinaryWriter"/> to write strings with.</param>
+		public void Assemble(BinaryWriter bw, BinaryWriter strw)
+		{
+			// Write CollectionName
+			strw.WriteNullTermUTF8(this._collection_name);
+			bw.WriteNullTermUTF8(this._collection_name, 0x20);
+
+			// Write settings
+			bw.Write(this.BinKey);
+			bw.WriteEnum(this.TakePhotoMethod);
+			bw.Write(this.BelongsToStage);
+			bw.Write(this.CashValue);
+			bw.Write(this.DescStringLabel.BinHash());
+			bw.Write(this.DestinationPoint.BinHash());
+			bw.Write((int)0);
+			bw.Write(this.Unknown0x34);
+			bw.Write(this.Unknown0x35);
+			bw.Write((short)0);
+			bw.Write(this.DescAttrib.BinHash());
+			bw.Write(this.RequiredVisualRating);
+		}
+
+		/// <summary>
+		/// Disassembles array into <see cref="GShowcase"/> properties.
+		/// </summary>
+		/// <param name="br"><see cref="BinaryReader"/> to read <see cref="GShowcase"/> with.</param>
+		public void Disassemble(BinaryReader br)
+		{
+			// Collection Name
+			this._collection_name = br.ReadNullTermUTF8(0x20);
+
+			// Take Photo Settings
+			this.TakePhotoMethod = br.ReadEnum<eTakePhotoMethod>();
+			this.BelongsToStage = br.ReadByte();
+			this.CashValue = br.ReadInt16();
+			this.DescStringLabel = br.ReadUInt32().BinString(eLookupReturn.EMPTY);
+			this.DestinationPoint = br.ReadUInt32().BinString(eLookupReturn.EMPTY);
+			br.BaseStream.Position += 4;
+			this.Unknown0x34 = br.ReadByte();
+			this.Unknown0x35 = br.ReadByte();
+			br.BaseStream.Position += 2;
+			this.DescAttrib = br.ReadUInt32().BinString(eLookupReturn.EMPTY);
+			this.RequiredVisualRating = br.ReadSingle();
+		}
+
+		/// <summary>
+		/// Casts all attributes from this object to another one.
+		/// </summary>
+		/// <param name="CName">CollectionName of the new created object.</param>
+		/// <returns>Memory casted copy of the object.</returns>
+		public override ACollectable MemoryCast(string CName)
+		{
+			var result = new GShowcase(CName, this.Database)
+			{
+				DescAttrib = this.DescAttrib,
+				DescStringLabel = this.DescStringLabel,
+				DestinationPoint = this.DestinationPoint,
+				TakePhotoMethod = this.TakePhotoMethod,
+				BelongsToStage = this.BelongsToStage,
+				CashValue = this.CashValue,
+				RequiredVisualRating = this.RequiredVisualRating,
+				Unknown0x34 = this.Unknown0x34,
+				Unknown0x35 = this.Unknown0x35
+			};
+
+			return result;
+		}
+
+		/// <summary>
+		/// Returns CollectionName, BinKey and GameSTR of this <see cref="GCareerBrand"/> 
+		/// as a string value.
+		/// </summary>
+		/// <returns>String value.</returns>
+		public override string ToString()
+		{
+			return $"Collection Name: {this.CollectionName} | " +
+				   $"BinKey: {this.BinKey.ToString("X8")} | Game: {this.GameSTR}";
+		}
+
+		#endregion
+	}
+}
