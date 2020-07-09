1f8caefee81d9843074dc9142761e8342481a082
MaxHwoy <max.hwoy@gmail.com>
2020-07-09 15:59:17 -0700

    Added SunInfo to Underground2 support

--- Nikki/Support.Underground2/Class/SunInfo.cs
+++ Nikki/Support.Underground2/Class/SunInfo.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.ComponentModel;
 using Nikki.Core;
 using Nikki.Utils;
+using Nikki.Reflection.Enum;
 using Nikki.Reflection.Abstract;
-using Nikki.Reflection.Exception;
 using Nikki.Reflection.Attributes;
+using Nikki.Support.Underground2.Framework;
 using Nikki.Support.Shared.Parts.SunParts;
 using CoreExtensions.IO;
+using CoreExtensions.Conversions;
 
 
 
@@ -43,35 +45,32 @@ namespace Nikki.Support.Underground2.Class
 		/// <summary>
 		/// Game to which the class belongs to.
 		/// </summary>
+		[Browsable(false)]
 		public override GameINT GameINT => GameINT.Underground2;
 
 		/// <summary>
 		/// Game string to which the class belongs to.
 		/// </summary>
+		[Browsable(false)]
 		public override string GameSTR => GameINT.Underground2.ToString();
 
 		/// <summary>
-		/// Database to which the class belongs to.
+		/// Manager to which the class belongs to.
 		/// </summary>
-		public Database.Underground2 Database { get; set; }
+		[Browsable(false)]
+		public SunInfoManager Manager { get; set; }
 
 		/// <summary>
 		/// Collection name of the variable.
 		/// </summary>
 		[AccessModifiable()]
+		[Category("Main")]
 		public override string CollectionName
 		{
 			get => this._collection_name;
 			set
 			{
-				if (string.IsNullOrWhiteSpace(value))
-					throw new ArgumentNullException("This value cannot be left empty.");
-				if (value.Contains(" "))
-					throw new Exception("CollectionName cannot contain whitespace.");
-				if (value.Length > MaxCNameLength)
-					throw new ArgumentLengthException(MaxCNameLength);
-				if (this.Database.SunInfos.FindCollection(value) != null)
-					throw new CollectionExistenceException(value);
+				this.Manager?.CreationCheck(value);
 				this._collection_name = value;
 			}
 		}
@@ -79,47 +78,57 @@ namespace Nikki.Support.Underground2.Class
 		/// <summary>
 		/// Binary memory hash of the collection name.
 		/// </summary>
+		[Category("Main")]
+		[TypeConverter(typeof(HexConverter))]
 		public override uint BinKey => this._collection_name.BinHash();
 
 		/// <summary>
 		/// Vault memory hash of the collection name.
 		/// </summary>
+		[Category("Main")]
+		[TypeConverter(typeof(HexConverter))]
 		public override uint VltKey => this._collection_name.VltHash();
 
 		/// <summary>
 		/// Sun layer 1.
 		/// </summary>
 		[Expandable("SunLayers")]
+		[Browsable(false)]
 		public SunLayer SUNLAYER1 { get; set; }
 
 		/// <summary>
 		/// Sun layer 2.
 		/// </summary>
 		[Expandable("SunLayers")]
+		[Browsable(false)]
 		public SunLayer SUNLAYER2 { get; set; }
 
 		/// <summary>
 		/// Sun layer 3.
 		/// </summary>
 		[Expandable("SunLayers")]
+		[Browsable(false)]
 		public SunLayer SUNLAYER3 { get; set; }
 
 		/// <summary>
 		/// Sun layer 4.
 		/// </summary>
 		[Expandable("SunLayers")]
+		[Browsable(false)]
 		public SunLayer SUNLAYER4 { get; set; }
 
 		/// <summary>
 		/// Sun layer 5.
 		/// </summary>
 		[Expandable("SunLayers")]
+		[Browsable(false)]
 		public SunLayer SUNLAYER5 { get; set; }
 
 		/// <summary>
 		/// Sun layer 6.
 		/// </summary>
 		[Expandable("SunLayers")]
+		[Browsable(false)]
 		public SunLayer SUNLAYER6 { get; set; }
 
 		#endregion
@@ -129,23 +138,18 @@ namespace Nikki.Support.Underground2.Class
 		/// <summary>
 		/// Initializes new instance of <see cref="SunInfo"/>.
 		/// </summary>
-		public SunInfo() { }
+		public SunInfo() => this.Initialize();
 
 		/// <summary>
 		/// Initializes new instance of <see cref="SunInfo"/>.
 		/// </summary>
 		/// <param name="CName">CollectionName of the new instance.</param>
-		/// <param name="db"><see cref="Database.Underground2"/> to which this instance belongs to.</param>
-		public SunInfo(string CName, Database.Underground2 db)
+		/// <param name="manager"><see cref="SunInfoManager"/> to which this instance belongs to.</param>
+		public SunInfo(string CName, SunInfoManager manager)
 		{
-			this.Database = db;
+			this.Manager = manager;
 			this.CollectionName = CName;
-			this.SUNLAYER1 = new SunLayer();
-			this.SUNLAYER2 = new SunLayer();
-			this.SUNLAYER3 = new SunLayer();
-			this.SUNLAYER4 = new SunLayer();
-			this.SUNLAYER5 = new SunLayer();
-			this.SUNLAYER6 = new SunLayer();
+			this.Initialize();
 			CName.BinHash();
 		}
 
@@ -153,16 +157,11 @@ namespace Nikki.Support.Underground2.Class
 		/// Initializes new instance of <see cref="SunInfo"/>.
 		/// </summary>
 		/// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
-		/// <param name="db"><see cref="Database.Underground2"/> to which this instance belongs to.</param>
-		public SunInfo(BinaryReader br, Database.Underground2 db)
+		/// <param name="manager"><see cref="SunInfoManager"/> to which this instance belongs to.</param>
+		public SunInfo(BinaryReader br, SunInfoManager manager)
 		{
-			this.Database = db;
-			this.SUNLAYER1 = new SunLayer();
-			this.SUNLAYER2 = new SunLayer();
-			this.SUNLAYER3 = new SunLayer();
-			this.SUNLAYER4 = new SunLayer();
-			this.SUNLAYER5 = new SunLayer();
-			this.SUNLAYER6 = new SunLayer();
+			this.Manager = manager;
+			this.Initialize();
 			this.Disassemble(br);
 		}
 
@@ -236,11 +235,21 @@ namespace Nikki.Support.Underground2.Class
 		/// </summary>
 		/// <param name="CName">CollectionName of the new created object.</param>
 		/// <returns>Memory casted copy of the object.</returns>
-		public override ACollectable MemoryCast(string CName)
+		public override Collectable MemoryCast(string CName)
 		{
-			var result = new SunInfo(CName, this.Database);
+			var result = new SunInfo(CName, this.Manager);
 			base.MemoryCast(this, result);
 			return result;
+		}
+
+		private void Initialize()
+		{
+			this.SUNLAYER1 = new SunLayer();
+			this.SUNLAYER2 = new SunLayer();
+			this.SUNLAYER3 = new SunLayer();
+			this.SUNLAYER4 = new SunLayer();
+			this.SUNLAYER5 = new SunLayer();
+			this.SUNLAYER6 = new SunLayer();
 		}
 
 		/// <summary>
@@ -251,7 +260,89 @@ namespace Nikki.Support.Underground2.Class
 		public override string ToString()
 		{
 			return $"Collection Name: {this.CollectionName} | " +
-				   $"BinKey: {this.BinKey.ToString("X8")} | Game: {this.GameSTR}";
+				   $"BinKey: {this.BinKey:X8} | Game: {this.GameSTR}";
+		}
+
+		#endregion
+
+		#region Serialization
+
+		/// <summary>
+		/// Serializes instance into a byte array and stores it in the file provided.
+		/// </summary>
+		/// <param name="bw"><see cref="BinaryWriter"/> to write data with.</param>
+		public override void Serialize(BinaryWriter bw)
+		{
+			byte[] array;
+			using (var ms = new MemoryStream(0xF5 + this._collection_name.Length))
+			using (var writer = new BinaryWriter(ms))
+			{
+
+				// Write CollectionName and Version
+				writer.WriteNullTermUTF8(this._collection_name);
+				writer.Write(this.Version);
+
+				// Write Positions
+				writer.Write(this.PositionX);
+				writer.Write(this.PositionY);
+				writer.Write(this.PositionZ);
+				writer.Write(this.CarShadowPositionX);
+				writer.Write(this.CarShadowPositionY);
+				writer.Write(this.CarShadowPositionZ);
+
+				// Write Layers
+				this.SUNLAYER1.Write(writer);
+				this.SUNLAYER2.Write(writer);
+				this.SUNLAYER3.Write(writer);
+				this.SUNLAYER4.Write(writer);
+				this.SUNLAYER5.Write(writer);
+				this.SUNLAYER6.Write(writer);
+
+				array = ms.ToArray();
+
+			}
+
+			array = Interop.Compress(array, eLZCompressionType.BEST);
+
+			var header = new SerializationHeader(array.Length, this.GameINT, this.Manager.Name);
+			header.Write(bw);
+			bw.Write(array.Length);
+			bw.Write(array);
+		}
+
+		/// <summary>
+		/// Deserializes byte array into an instance by loading data from the file provided.
+		/// </summary>
+		/// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
+		public override void Deserialize(BinaryReader br)
+		{
+			int size = br.ReadInt32();
+			var array = br.ReadBytes(size);
+
+			array = Interop.Decompress(array);
+
+			using var ms = new MemoryStream(array);
+			using var reader = new BinaryReader(ms);
+
+			// Read CollectionName and Version
+			this._collection_name = reader.ReadNullTermUTF8();
+			this.Version = reader.ReadInt32();
+
+			// Read Positions
+			this.PositionX = reader.ReadSingle();
+			this.PositionY = reader.ReadSingle();
+			this.PositionZ = reader.ReadSingle();
+			this.CarShadowPositionX = reader.ReadSingle();
+			this.CarShadowPositionY = reader.ReadSingle();
+			this.CarShadowPositionZ = reader.ReadSingle();
+
+			// Read Layers
+			this.SUNLAYER1.Read(reader);
+			this.SUNLAYER2.Read(reader);
+			this.SUNLAYER3.Read(reader);
+			this.SUNLAYER4.Read(reader);
+			this.SUNLAYER5.Read(reader);
+			this.SUNLAYER6.Read(reader);
 		}
 
 		#endregion
