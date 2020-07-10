297561d533c157143ad6220664dc5bd8c876c1a7
MaxHwoy <max.hwoy@gmail.com>
2020-07-10 04:05:13 -0700

    Added SlotOverride and SlotType classes to Underground2 support

---
+++ Nikki/Support.Underground2/Class/SlotOverride.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel;
+using Nikki.Core;
+using Nikki.Utils;
+using Nikki.Reflection.Enum;
+using Nikki.Reflection.Abstract;
+using Nikki.Reflection.Attributes;
+using Nikki.Reflection.Enum.SlotID;
+using Nikki.Support.Underground2.Framework;
+using CoreExtensions.IO;
+using CoreExtensions.Conversions;
+
+
+
+namespace Nikki.Support.Underground2.Class
+{
+    /// <summary>
+    /// <see cref="SlotOverride"/> is a collection of settings related to car's slot overrides.
+    /// </summary>
+    public class SlotOverride : Shared.Class.SlotOverride
+    {
+        #region Fields
+
+        private string _collection_name;
+
+        /// <summary>
+        /// Maximum length of the CollectionName.
+        /// </summary>
+        public const int MaxCNameLength = -1;
+
+        /// <summary>
+        /// Offset of the CollectionName in the data.
+        /// </summary>
+        public const int CNameOffsetAt = -1;
+
+        /// <summary>
+        /// Base size of a unit collection.
+        /// </summary>
+        public const int BaseClassSize = 0x10;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Game to which the class belongs to.
+        /// </summary>
+        [Browsable(false)]
+        public override GameINT GameINT => GameINT.Underground2;
+
+        /// <summary>
+        /// Game string to which the class belongs to.
+        /// </summary>
+        [Browsable(false)]
+        public override string GameSTR => GameINT.Underground2.ToString();
+
+        /// <summary>
+        /// Manager to which the class belongs to.
+        /// </summary>
+        [Browsable(false)]
+        public SlotOverrideManager Manager { get; set; }
+
+        /// <summary>
+        /// Collection name of the variable.
+        /// </summary>
+        [AccessModifiable()]
+        [Category("Main")]
+        public override string CollectionName
+        {
+            get => this._collection_name;
+            set
+            {
+                this.Manager?.CreationCheck(value);
+                this._collection_name = value;
+            }
+        }
+
+        /// <summary>
+        /// Binary memory hash of the collection name.
+        /// </summary>
+        [Category("Main")]
+        [TypeConverter(typeof(HexConverter))]
+        public override uint BinKey => this._collection_name.BinHash();
+
+        /// <summary>
+        /// Vault memory hash of the collection name.
+        /// </summary>
+        [Category("Main")]
+        [TypeConverter(typeof(HexConverter))]
+        public override uint VltKey => this._collection_name.VltHash();
+
+        #endregion
+
+        #region Main
+
+        /// <summary>
+        /// Initializes new instance of <see cref="SlotOverride"/>.
+        /// </summary>
+        public SlotOverride() { }
+
+        /// <summary>
+        /// Initializes new instance of <see cref="SlotOverride"/>.
+        /// </summary>
+        /// <param name="CName">CollectionName of the new instance.</param>
+        /// <param name="manager"><see cref="SlotOverrideManager"/> to which this instance belongs to.</param>
+        public SlotOverride(string CName, SlotOverrideManager manager)
+        {
+            this.Manager = manager;
+            this.CollectionName = CName;
+            CName.BinHash();
+        }
+
+        /// <summary>
+        /// Initializes new instance of <see cref="SlotOverride"/>.
+        /// </summary>
+        /// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
+        /// <param name="manager"><see cref="SlotOverrideManager"/> to which this instance belongs to.</param>
+        public SlotOverride(BinaryReader br, SlotOverrideManager manager)
+        {
+            this.Manager = manager;
+            this.Disassemble(br);
+        }
+
+        /// <summary>
+        /// Destroys current instance.
+        /// </summary>
+        ~SlotOverride() { }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Assembles <see cref="SlotType"/> into a byte array.
+        /// </summary>
+        /// <param name="bw"><see cref="BinaryWriter"/> to write <see cref="SlotType"/> with.</param>
+        public override void Assemble(BinaryWriter bw)
+        {
+            var keys = this._collection_name.Split("_PART_", 2, StringSplitOptions.None);
+            var id = (eSlotUnderground2)Enum.Parse(typeof(eSlotUnderground2), keys[1]);
+
+            bw.Write(keys[0].BinHash());
+            bw.Write((int)id);
+            bw.Write(keys[0].BinHash());
+            bw.Write(this.InfoMainOverride.BinHash());
+        }
+
+        /// <summary>
+        /// Disassembles array into <see cref="SlotType"/> properties.
+        /// </summary>
+        /// <param name="br"><see cref="BinaryReader"/> to read <see cref="SlotType"/> with.</param>
+        public override void Disassemble(BinaryReader br)
+        {
+            var key = br.ReadUInt32().BinString(eLookupReturn.EMPTY);
+            var id = (eSlotUnderground2)br.ReadInt32();
+            br.BaseStream.Position += 4;
+            this._collection_name = $"{key}_PART_{id}";
+
+            this.InfoMainOverride = br.ReadUInt32().BinString(eLookupReturn.EMPTY);
+        }
+
+        /// <summary>
+        /// Casts all attributes from this object to another one.
+        /// </summary>
+        /// <param name="CName">CollectionName of the new created object.</param>
+        /// <returns>Memory casted copy of the object.</returns>
+        public override Collectable MemoryCast(string CName)
+        {
+            var result = new SlotOverride(CName, this.Manager);
+            base.MemoryCast(this, result);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns CollectionName, BinKey and GameSTR of this <see cref="SlotOverride"/> 
+        /// as a string value.
+        /// </summary>
+        /// <returns>String value.</returns>
+        public override string ToString()
+        {
+            return $"Collection Name: {this.CollectionName} | " +
+                   $"BinKey: {this.BinKey:X8} | Game: {this.GameSTR}";
+        }
+
+        #endregion
+
+        #region Serialization
+
+        /// <summary>
+        /// Serializes instance into a byte array and stores it in the file provided.
+        /// </summary>
+        /// <param name="bw"><see cref="BinaryWriter"/> to write data with.</param>
+        public override void Serialize(BinaryWriter bw)
+        {
+            byte[] array;
+            using (var ms = new MemoryStream(0x60))
+            using (var writer = new BinaryWriter(ms))
+            {
+
+                writer.WriteNullTermUTF8(this._collection_name);
+                writer.WriteNullTermUTF8(this.InfoMainOverride);
+
+                array = ms.ToArray();
+
+            }
+
+            array = Interop.Compress(array, eLZCompressionType.RAWW);
+
+            var header = new SerializationHeader(array.Length, this.GameINT, this.Manager.Name);
+            header.Write(bw);
+            bw.Write(array.Length);
+            bw.Write(array);
+        }
+
+        /// <summary>
+        /// Deserializes byte array into an instance by loading data from the file provided.
+        /// </summary>
+        /// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
+        public override void Deserialize(BinaryReader br)
+        {
+            int size = br.ReadInt32();
+            var array = br.ReadBytes(size);
+
+            array = Interop.Decompress(array);
+
+            using var ms = new MemoryStream(array);
+            using var reader = new BinaryReader(ms);
+
+            this._collection_name = reader.ReadNullTermUTF8();
+            this.InfoMainOverride = reader.ReadNullTermUTF8();
+        }
+
+        #endregion
+    }
+}
---
+++ Nikki/Support.Underground2/Class/SlotType.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel;
+using Nikki.Core;
+using Nikki.Utils;
+using Nikki.Reflection.Enum;
+using Nikki.Reflection.Abstract;
+using Nikki.Reflection.Attributes;
+using Nikki.Reflection.Enum.SlotID;
+using Nikki.Support.Underground2.Framework;
+using CoreExtensions.IO;
+using CoreExtensions.Conversions;
+
+
+
+namespace Nikki.Support.Underground2.Class
+{
+    /// <summary>
+    /// <see cref="SlotType"/> is a collection of settings related to a parts and slot information.
+    /// </summary>
+    public class SlotType : Shared.Class.SlotType
+    {
+        #region Fields
+
+        private string _collection_name;
+        private static int _index;
+
+        /// <summary>
+        /// Maximum length of the CollectionName.
+        /// </summary>
+        public const int MaxCNameLength = -1;
+
+        /// <summary>
+        /// Offset of the CollectionName in the data.
+        /// </summary>
+        public const int CNameOffsetAt = -1;
+
+        /// <summary>
+        /// Base size of a unit collection.
+        /// </summary>
+        public const int BaseClassSize = 0x8;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Game to which the class belongs to.
+        /// </summary>
+        [Browsable(false)]
+        public override GameINT GameINT => GameINT.Underground2;
+
+        /// <summary>
+        /// Game string to which the class belongs to.
+        /// </summary>
+        [Browsable(false)]
+        public override string GameSTR => GameINT.Underground2.ToString();
+
+        /// <summary>
+        /// Manager to which the class belongs to.
+        /// </summary>
+        [Browsable(false)]
+        public SlotTypeManager Manager { get; set; }
+
+        /// <summary>
+        /// Collection name of the variable.
+        /// </summary>
+        [ReadOnly(true)]
+        [Category("Main")]
+        public override string CollectionName
+        {
+            get => this._collection_name;
+            set
+            {
+                this.Manager?.CreationCheck(value);
+                this._collection_name = value;
+            }
+        }
+
+        /// <summary>
+        /// Binary memory hash of the collection name.
+        /// </summary>
+        [Category("Main")]
+        [TypeConverter(typeof(HexConverter))]
+        public override uint BinKey => this._collection_name.BinHash();
+
+        /// <summary>
+        /// Vault memory hash of the collection name.
+        /// </summary>
+        [Category("Main")]
+        [TypeConverter(typeof(HexConverter))]
+        public override uint VltKey => this._collection_name.VltHash();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [AccessModifiable()]
+        [MemoryCastable()]
+        [Category("Primary")]
+        public eCarAnimLocation SecondaryAnimation { get; set; } = eCarAnimLocation.CARANIM_NONE;
+
+        /// <summary>
+        /// Current index of this <see cref="SlotType"/>.
+        /// </summary>
+        internal static int Index
+        {
+            get => _index;
+            set => _index = value >= 170 ? 0 : value;
+        }
+
+        #endregion
+
+        #region Main
+
+        /// <summary>
+        /// Initializes new instance of <see cref="SlotType"/>.
+        /// </summary>
+        public SlotType() { }
+
+        /// <summary>
+        /// Initializes new instance of <see cref="SlotType"/>.
+        /// </summary>
+        /// <param name="CName">CollectionName of the new instance.</param>
+        /// <param name="manager"><see cref="SlotTypeManager"/> to which this instance belongs to.</param>
+        public SlotType(string CName, SlotTypeManager manager)
+        {
+            this.Manager = manager;
+            this.CollectionName = CName;
+            CName.BinHash();
+        }
+
+        /// <summary>
+        /// Initializes new instance of <see cref="SlotType"/>.
+        /// </summary>
+        /// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
+        /// <param name="manager"><see cref="SlotTypeManager"/> to which this instance belongs to.</param>
+        public SlotType(BinaryReader br, SlotTypeManager manager)
+        {
+            this.Manager = manager;
+            this.Disassemble(br);
+        }
+
+        /// <summary>
+        /// Destroys current instance.
+        /// </summary>
+        ~SlotType() { }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Assembles <see cref="SlotType"/> into a byte array.
+        /// </summary>
+        /// <param name="bw"><see cref="BinaryWriter"/> to write <see cref="SlotType"/> with.</param>
+        public override void Assemble(BinaryWriter bw)
+        {
+            bw.Write(String.IsNullOrEmpty(this.SlotStockOverride)
+                ? 0xFFFFFFFF
+                : this.SlotStockOverride.BinHash());
+            bw.Write(this.SlotMainOverride.BinHash());
+        }
+
+        /// <summary>
+        /// Disassembles array into <see cref="SlotType"/> properties.
+        /// </summary>
+        /// <param name="br"><see cref="BinaryReader"/> to read <see cref="SlotType"/> with.</param>
+        public override void Disassemble(BinaryReader br)
+        {
+            this._collection_name = ((eSlotUnderground2)(Index++)).ToString();
+
+            uint key = 0;
+            const uint empty = 0xFFFFFFFF;
+
+            key = br.ReadUInt32();
+            this.SlotStockOverride = key == empty ? String.Empty : key.BinString(eLookupReturn.EMPTY);
+
+            this.SlotMainOverride = br.ReadUInt32().BinString(eLookupReturn.EMPTY);
+        }
+
+        /// <summary>
+        /// Casts all attributes from this object to another one.
+        /// </summary>
+        /// <param name="CName">CollectionName of the new created object.</param>
+        /// <returns>Memory casted copy of the object.</returns>
+        public override Collectable MemoryCast(string CName)
+        {
+            var result = new SlotType(CName, this.Manager);
+            base.MemoryCast(this, result);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns CollectionName, BinKey and GameSTR of this <see cref="SlotType"/> 
+        /// as a string value.
+        /// </summary>
+        /// <returns>String value.</returns>
+        public override string ToString()
+        {
+            return $"Collection Name: {this.CollectionName} | " +
+                   $"BinKey: {this.BinKey:X8} | Game: {this.GameSTR}";
+        }
+
+        #endregion
+
+        #region Serialization
+
+        /// <summary>
+        /// Serializes instance into a byte array and stores it in the file provided.
+        /// </summary>
+        /// <param name="bw"><see cref="BinaryWriter"/> to write data with.</param>
+        public override void Serialize(BinaryWriter bw)
+        {
+            byte[] array;
+            using (var ms = new MemoryStream(0x60))
+            using (var writer = new BinaryWriter(ms))
+            {
+
+                writer.WriteNullTermUTF8(this._collection_name);
+                writer.WriteNullTermUTF8(this.SlotStockOverride);
+                writer.WriteNullTermUTF8(this.SlotMainOverride);
+                writer.WriteEnum(this.PrimaryAnimation);
+                writer.WriteEnum(this.SecondaryAnimation);
+
+                array = ms.ToArray();
+
+            }
+
+            array = Interop.Compress(array, eLZCompressionType.RAWW);
+
+            var header = new SerializationHeader(array.Length, this.GameINT, this.Manager.Name);
+            header.Write(bw);
+            bw.Write(array.Length);
+            bw.Write(array);
+        }
+
+        /// <summary>
+        /// Deserializes byte array into an instance by loading data from the file provided.
+        /// </summary>
+        /// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
+        public override void Deserialize(BinaryReader br)
+        {
+            int size = br.ReadInt32();
+            var array = br.ReadBytes(size);
+
+            array = Interop.Decompress(array);
+
+            using var ms = new MemoryStream(array);
+            using var reader = new BinaryReader(ms);
+
+            this._collection_name = reader.ReadNullTermUTF8();
+            this.SlotStockOverride = reader.ReadNullTermUTF8();
+            this.SlotMainOverride = reader.ReadNullTermUTF8();
+            this.PrimaryAnimation = reader.ReadEnum<eCarAnimLocation>();
+            this.SecondaryAnimation = reader.ReadEnum<eCarAnimLocation>();
+        }
+
+        #endregion
+    }
+}
