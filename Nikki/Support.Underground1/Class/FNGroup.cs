35ce1b9dceb197f00f46955e5ff5b15029d0f631
MaxHwoy <max.hwoy@gmail.com>
2020-08-31 16:20:54 -0700

    Fixed FNGroup in Underground1 support

--- Nikki/Support.Underground1/Class/FNGroup.cs
+++ Nikki/Support.Underground1/Class/FNGroup.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.ComponentModel;
 using Nikki.Core;
+using Nikki.Utils;
 using Nikki.Utils.EA;
-using Nikki.Reflection.ID;
+using Nikki.Reflection.Enum;
 using Nikki.Support.Shared.Parts.FNGParts;
+using Nikki.Support.Underground1.Framework;
 using CoreExtensions.IO;
 using CoreExtensions.Conversions;
 
@@ -16,27 +19,37 @@ namespace Nikki.Support.Underground1.Class
     /// </summary>
     public class FNGroup : Shared.Class.FNGroup
     {
-		#region Properties
+        #region Fields
+
+        private byte[] _data;
+
+        #endregion
+
+        #region Properties
 
         /// <summary>
         /// Actual data of this <see cref="FNGroup"/>.
         /// </summary>
-		public byte[] Data { get; private set; }
+        [Browsable(false)]
+        public byte[] Data => this._data;
 
         /// <summary>
         /// Game to which the class belongs to.
         /// </summary>
+        [Browsable(false)]
         public override GameINT GameINT => GameINT.Underground1;
 
         /// <summary>
         /// Game string to which the class belongs to.
         /// </summary>
+        [Browsable(false)]
         public override string GameSTR => GameINT.Underground1.ToString();
 
         /// <summary>
-        /// Database to which the class belongs to.
+        /// Manager to which the class belongs to.
         /// </summary>
-        public Database.Underground1 Database { get; set; }
+        [Browsable(false)]
+        public FNGroupManager Manager { get; set; }
 
         #endregion
 
@@ -51,10 +64,10 @@ namespace Nikki.Support.Underground1.Class
         /// Initializes new instance of <see cref="FNGroup"/>.
         /// </summary>
         /// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
-        /// <param name="db"><see cref="Database.Underground1"/> to which this instance belongs to.</param>
-        public FNGroup(BinaryReader br, Database.Underground1 db)
+        /// <param name="manager"><see cref="FNGroupManager"/> to which this instance belongs to.</param>
+        public FNGroup(BinaryReader br, FNGroupManager manager)
         {
-            this.Database = db;
+            this.Manager = manager;
             this.Disassemble(br);
         }
 
@@ -73,24 +86,24 @@ namespace Nikki.Support.Underground1.Class
         /// <param name="bw"><see cref="BinaryWriter"/> to write <see cref="FNGroup"/> with.</param>
         public override void Assemble(BinaryWriter bw)
         {
-            using var ms = new MemoryStream(this.Data);
+            using var ms = new MemoryStream(this._data);
             using var writer = new BinaryWriter(ms);
 
-            bw.Write(Global.FEngFiles);
-            bw.Write(this.Data.Length);
+            bw.WriteEnum(BinBlockID.FEngFiles);
+            bw.Write(this._data.Length);
 
             foreach (var color in this._colorinfo)
             {
-               
+
                 writer.BaseStream.Position = color.Offset;
                 writer.Write((uint)color.Blue);
                 writer.Write((uint)color.Green);
                 writer.Write((uint)color.Red);
                 writer.Write((uint)color.Alpha);
-            
+
             }
 
-            bw.Write(this.Data);
+            bw.Write(this._data);
         }
 
         /// <summary>
@@ -102,47 +115,46 @@ namespace Nikki.Support.Underground1.Class
             var ID = br.ReadUInt32();
             var size = br.ReadInt32();
 
-            this.Data = SAT.Decompress(br.ReadBytes(size), ID);
+            this._data = SAT.Decompress(br.ReadBytes(size), ID);
 
-            using var ms = new MemoryStream(this.Data);
+            using var ms = new MemoryStream(this._data);
             using var reader = new BinaryReader(ms);
 
             reader.BaseStream.Position = 0x28;
-            this.CollectionName = reader.ReadNullTermUTF8();
-            
-            if (this.CollectionName.EndsWith(".fng"))
+            this.CollectionName = reader.ReadNullTermUTF8().ToUpper();
+
+            if (this.CollectionName.EndsWith(".FNG"))
             {
-            
-                this.CollectionName.GetFormattedValue("{X}.fng", out string name);
+
+                this.CollectionName.GetFormattedValue("{X}.FNG", out string name);
                 this.CollectionName = name;
-            
+
             }
 
             reader.BaseStream.Position = 0x28;
-            
+
             while (reader.BaseStream.Position < reader.BaseStream.Length)
             {
-            
+
                 byte b1 = reader.ReadByte();
                 byte b2 = reader.ReadByte();
                 byte b3 = reader.ReadByte();
                 byte b4 = reader.ReadByte();
 
                 // SAT, SAD, SA(0x90) or 1111
-                if ((b1 == 'S' && b2 == 'A') || (b1 == Byte.MaxValue && b2 == Byte.MaxValue && 
+                if ((b1 == 'S' && b2 == 'A') || (b1 == Byte.MaxValue && b2 == Byte.MaxValue &&
                      b3 == Byte.MaxValue && b4 == Byte.MaxValue))
                 {
-                
+
                     uint Offset = (uint)reader.BaseStream.Position;
                     uint Blue = reader.ReadUInt32();
                     uint Green = reader.ReadUInt32();
                     uint Red = reader.ReadUInt32();
                     uint Alpha = reader.ReadUInt32();
-                    
-                    if (Blue <= Byte.MaxValue && Green <= Byte.MaxValue && 
+                    if (Blue <= Byte.MaxValue && Green <= Byte.MaxValue &&
                         Red <= Byte.MaxValue && Alpha <= Byte.MaxValue)
                     {
-                    
+
                         var color = new FEngColor(this)
                         {
                             Offset = Offset,
@@ -151,13 +163,13 @@ namespace Nikki.Support.Underground1.Class
                             Red = (byte)Red,
                             Alpha = (byte)Alpha
                         };
-                        
+
                         this._colorinfo.Add(color);
-                    
+
                     }
-                
+
                 }
-            
+
             }
         }
 
@@ -169,7 +181,90 @@ namespace Nikki.Support.Underground1.Class
         public override string ToString()
         {
             return $"Collection Name: {this.CollectionName} | " +
-                   $"BinKey: {this.BinKey.ToString("X8")} | Game: {this.GameSTR}";
+                   $"BinKey: {this.BinKey:X8} | Game: {this.GameSTR}";
+        }
+
+        #endregion
+
+        #region Serialization
+
+        /// <summary>
+        /// Serializes instance into a byte array and stores it in the file provided.
+        /// </summary>
+        /// <param name="bw"><see cref="BinaryWriter"/> to write data with.</param>
+        public override void Serialize(BinaryWriter bw)
+        {
+            byte[] array;
+            var size = this._data.Length + (this.FEngColorCount << 3) + this.CollectionName.Length + 0x20;
+            using (var ms = new MemoryStream(size))
+            using (var writer = new BinaryWriter(ms))
+            {
+
+                writer.WriteNullTermUTF8(this.CollectionName);
+                writer.Write(this.FEngColorCount);
+
+                foreach (var color in this._colorinfo)
+                {
+
+                    writer.Write(color.Offset);
+                    writer.Write(color.Alpha);
+                    writer.Write(color.Red);
+                    writer.Write(color.Green);
+                    writer.Write(color.Blue);
+
+                }
+
+                writer.Write(this._data.Length);
+                writer.Write(this._data);
+
+                array = ms.ToArray();
+
+            }
+
+            array = Interop.Compress(array, LZCompressionType.BEST);
+
+            var header = new SerializationHeader(array.Length, this.GameINT, this.Manager.Name);
+            header.Write(bw);
+            bw.Write(array.Length);
+            bw.Write(array);
+        }
+
+        /// <summary>
+        /// Deserializes byte array into an instance by loading data from the file provided.
+        /// </summary>
+        /// <param name="br"><see cref="BinaryReader"/> to read data with.</param>
+        public override void Deserialize(BinaryReader br)
+        {
+            int size = br.ReadInt32();
+            var array = br.ReadBytes(size);
+
+            array = Interop.Decompress(array);
+
+            using var ms = new MemoryStream(array);
+            using var reader = new BinaryReader(ms);
+
+            this.CollectionName = reader.ReadNullTermUTF8();
+            var count = reader.ReadInt32();
+            this._colorinfo.Capacity = count;
+
+            for (int loop = 0; loop < count; ++loop)
+            {
+
+                var color = new FEngColor(this)
+                {
+                    Offset = reader.ReadUInt32(),
+                    Alpha = reader.ReadByte(),
+                    Red = reader.ReadByte(),
+                    Green = reader.ReadByte(),
+                    Blue = reader.ReadByte()
+                };
+
+                this._colorinfo.Add(color);
+
+            }
+
+            count = reader.ReadInt32();
+            this._data = reader.ReadBytes(count);
         }
 
         #endregion
